c00fcd82f2f72aedcb5be7ea437bc406903196aa
ahmedneil2 <91541696+ahmedneil2@users.noreply.github.com>
2025-03-30 12:31:40 +0600

    Update media.py

--- api/media.py
+++ api/media.py
@@ -1,5 +1,5 @@
-from database.ia_filterdb import col, sec_col
 from aiohttp import web
+from database.ia_filterdb import col, sec_col
 
 async def list_media(request):
     try:
@@ -18,12 +18,27 @@ async def list_media(request):
         cursor2 = sec_col.find({}).sort('file_name', 1)
         async for file in cursor2:
             files.append({
-                'file_id': file['file_id'], 
+                'file_id': file['file_id'],
                 'file_name': file['file_name'],
                 'file_size': file['file_size']
             })
             
-        return web.json_response({'files': files})
+        return web.json_response(
+            {'files': files},
+            headers={
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET',
+                'Access-Control-Allow-Headers': 'Content-Type'
+            }
+        )
         
     except Exception as e:
-        return web.json_response({'error': str(e)}, status=500)
+        return web.json_response(
+            {'error': str(e)},
+            status=500,
+            headers={
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET',
+                'Access-Control-Allow-Headers': 'Content-Type'
+            }
+        )
